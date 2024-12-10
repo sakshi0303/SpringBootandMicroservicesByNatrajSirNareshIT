6ebe045daa0bd90ed6fd4e02e1a2a30875991638
sakshi0303 <65711308+sakshi0303@users.noreply.github.com>
2024-12-10 17:29:05 -0600

    Delete BootJpaProj01-CrudRepository directory

--- BootJpaProj01-CrudRepository/src/main/java/com/nt/repository/IArtistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nt.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.nt.entity.Artist;
-
-public interface IArtistRepository extends CrudRepository<Artist, Integer> {
-
-}
--- BootJpaProj01-CrudRepository/src/main/java/com/nt/service/IArtistMgmtService.java
+++
@@ -1,9 +0,0 @@
-package com.nt.service;
-
-import com.nt.entity.Artist;
-
-public interface IArtistMgmtService {
-   public  String  registerArtist(Artist artist);
-   public    boolean  checkArtistAvailability(int  id);
-   public   long    showArtistsCount();
-}
